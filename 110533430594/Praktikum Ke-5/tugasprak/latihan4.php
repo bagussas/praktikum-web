fa66464c604c8e48c2f7cfd28ea14f948f3ac937
Bagus Eka Sasmitha <bagoes.hunter@gmail.com>
2013-11-07 09:05:27 +0700

    Praktikum Ke-5

---
+++ 110533430594/Praktikum Ke-5/tugasprak/latihan4.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['nama'])){
+		header("location:login.php");
+	}
+	ini_set('display_errors',1);
+	// Meng-include file koneksi dan data handler
+	require_once './koneksi.php';
+	require_once './data_handler.php';
+	// Konstanta nama tabel
+	define('MHS', 'mahasiswa');
+	// Memanggil fungsi data handler
+	data_handler('?m=data');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Akses dan Manipulasi Data</title>
+		<style type="text/css">
+			.even {
+				background: #ddd;
+			}
+		</style>
+	</head>
+<body>
+
+</body>
+</html>
---
+++ 110533430594/Praktikum Ke-5/tugasprak/login_code.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	include "koneksi.php";
+	$user = $_POST['nama'];
+	$pass = $_POST['pass'];
+		
+	$select = "SELECT nim,nama FROM mahasiswa WHERE nim='$pass' and nama='$user'";
+	$query_login = mysql_query($select);
+	if($query_login){
+		$_SESSION['nama'] = $_POST['nama'];
+		header("location:latihan4.php");
+	}else{
+		?>
+			<script language="javascript">
+				alert("maaf, username atau password anda tidak cocok");
+				document.location="login.php";
+			</script>
+		<?php
+	}
+?>
